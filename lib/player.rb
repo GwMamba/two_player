e2f99c457a07aa6011c9a8e0aa45e1bdcbf47334
GwMamba <gerald.mwang@gmail.com>
2023-12-19 21:09:55 +0900

    two player repo initialization.

---
+++ lib/player.rb
@@ -0,0 +1,10 @@
+class Player
+  attr_reader :name, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def answer_question(answer)
+    # logic to handle answering questions and updating lives
